7a183c13003ad7421ca0b58c8cb59d29c26d733a
uzimasam <uzimasamuel1@gmail.com>
2025-01-19 21:14:55 +0300

    refactor: Organize routes into groups for lecturers, students, schedule, attendance, unit, and program

--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,43 @@ Route::get('/', [DashboardController::class,'landing'])->name('landing');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
-    Route::get('lecturers', [DashboardController::class, 'printLecturers'])->name('printing.lecturers');
-    Route::post('lecturers/addCard', [DashboardController::class, 'addCard'])->name('lecturers.addCard');
-    Route::post('lecturers/swapCard', [DashboardController::class, 'swapCard'])->name('lecturers.swapCard');
-    Route::get('students', [DashboardController::class, 'printStudents'])->name('printing.students');
-    Route::post('students/addCard', [DashboardController::class,'addStudentCard'])->name('students.addCard');
-    Route::post('students/swapCard', [DashboardController::class,'swapStudentCard'])->name('students.swapCard');
+    Route::group(['prefix'=> 'lecturers'], function () {
+        Route::get('/', [DashboardController::class, 'printLecturers'])->name('printing.lecturers');
+        Route::post('/addCard', [DashboardController::class, 'addCard'])->name('lecturers.addCard');
+        Route::post('/swapCard', [DashboardController::class, 'swapCard'])->name('lecturers.swapCard');
+    });
+    Route::group(['prefix'=> 'students'], function () {
+        Route::get('/', [DashboardController::class, 'printStudents'])->name('printing.students');
+        Route::post('/addCard', [DashboardController::class,'addStudentCard'])->name('students.addCard');
+        Route::post('/swapCard', [DashboardController::class,'swapStudentCard'])->name('students.swapCard');
+        Route::post('/store', [StudentController::class, 'store'])->name('students.store');
+    });
     Route::group(['prefix'=> 'profile'], function () {
         Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        Route::get('/schedule', [ScheduleController::class, 'index'])->name('schedule');
     });
-    Route::post('/schedule/store', [ScheduleController::class, 'store'])->name('schedule.store');
-    Route::post('/schedule/reschedule', [ScheduleController::class, 'reschedule'])->name('schedule.reschedule');
-    Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance');
-    Route::get('/attendance/{attendance_link}', [AttendanceController::class, 'portal'])->name('attendance.portal');
-    Route::post('/attendance/store', [AttendanceController::class, 'store'])->name('attendances.store');
-    Route::post('/attendance/mark', [AttendanceController::class, 'mark'])->name('attendance.mark');
-    Route::get('/unit', [UnitController::class, 'index'])->name('unit');
-    Route::post('/unit/store', [UnitController::class, 'store'])->name('unit.store');
-    Route::get('/program', [ProgramController::class, 'index'])->name('program');
-    Route::post('/program/store', [ProgramController::class, 'store'])->name('program.store');
+    Route::group(['prefix'=> 'schedule'], function () {
+        Route::get('/', [ScheduleController::class, 'index'])->name('schedule');
+        Route::post('/store', [ScheduleController::class, 'store'])->name('schedule.store');
+        Route::post('/reschedule', [ScheduleController::class, 'reschedule'])->name('schedule.reschedule');
+    });
+    Route::group(['prefix'=> 'attendance'], function () {
+        Route::get('/', [AttendanceController::class, 'index'])->name('attendance');
+        Route::get('/{attendance_link}', [AttendanceController::class, 'portal'])->name('attendance.portal');
+        Route::post('/store', [AttendanceController::class, 'store'])->name('attendances.store');
+        Route::post('/mark', [AttendanceController::class, 'mark'])->name('attendance.mark');
+    });
+    Route::group(['prefix'=> 'unit'], function () {
+        Route::get('/', [UnitController::class, 'index'])->name('unit');
+        Route::post('/store', [UnitController::class, 'store'])->name('unit.store');
+    });
+    Route::group(['prefix'=> 'program'], function () {
+        Route::get('/', [ProgramController::class, 'index'])->name('program');
+        Route::post('/store', [ProgramController::class, 'store'])->name('program.store');
+    });
     Route::get('/setup', [ProgramController::class, 'setup'])->name('setup');
     Route::post('/school/store', [SchoolController::class, 'store'])->name('school.store');
-    Route::post('/students/store', [StudentController::class, 'store'])->name('students.store');
 });
 
 require __DIR__.'/auth.php';
